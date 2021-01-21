c630e666ff2ce2f5f155c27696638cc286560eb0
wiz4host <31190285+wiz4host@users.noreply.github.com>
2021-01-21 08:37:49 +0530

    Update awscli-install.sh
    add code for ubuntu OS as well and Python version selection is now "variablized"

--- shell_scripts/awscli-install.sh
+++ shell_scripts/awscli-install.sh
@@ -1,33 +1,86 @@
-######################################################
-#     This script runs at Redhat/centos platform     #
-#     It install aws-cli using python3.8 and pip3.8  #
-######################################################
+#########################################################################
+#     This script runs at Redhat/centos and Ubuntu Systems platform     #
+#     It install aws-cli using python3.8 and pip3.8                     #
+#########################################################################
 
 #!/bin/bash
 # Install AWS-CLI with Python3
-yum install gcc openssl-devel bzip2-devel libffi-devel -y
+
+PythonVer=3.6.7
+PythonVerMajor=PythonVerMajor=$(echo $PythonVer|cut -c -3)
+echo $PythonVerMajor
+
+
+#STEP1: Install dependency for baking python source code
+pkgarrcentos=( gcc openssl-devel bzip2-devel libffi-devel )
+pkgarrubuntu=( build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev wget libbz2-dev )
+updaterepo="y"
+
+#grep linux  OS  type
+oslinux=$(cat /etc/*-release| grep -w 'NAME='|awk -F "=" '{print $2}'|sed -e 's/^"//' -e 's/"$//'|awk -F " " '{print $1}')
+
+#convert $os  all in lowercase
+os="${oslinux,,}"
+
+
+#compare for ubuntu or centos
+if [[ $os == "ubuntu" ]]; then
+   osinstaller="apt-get"
+   pkgarr=${pkgarrubuntu[@]}
+fi
+
+if [ $os == "centos" ]; then
+   osinstaller="yum"
+   pkgarr=${pkgarrcentos[@]}
+fi
+
+echo "===================================================================="
+echo "Linux OS = $os"
+echo "package to be installed = ${pkgarr[@]}"
+echo "===================================================================="
+
+
+echo  ++++++++++++++++++++++++++++++++++++++++++ UPDATE Packages and Repos++++++++++++++++++++++++++++++
+if [[ $updaterepo == "y" ]]; then
+    sudo $osinstaller update -y
+fi
+
+echo +++++++++++++++++++++++++++++++++++++++++++INSTALL Package++++++++++++++++++++++++++++
+
+for pkg in ${pkgarr[@]}
+ do
+  sudo $osinstaller -y install $pkg|grep -w 'Package'
+ done
+
+updatedb
+mkdir -p /opt/lab/shell_script
+
+
+#Step2: Install and configure Python3
 cd /opt/
-curl -O https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz
-tar -xzf Python-3.8.1.tgz
-cd Python-3.8.1/
-./configure --enable-optimizations
-make altinstall
-python3.8 --version
+sudo curl -O https://www.python.org/ftp/python/${PythonVer}/Python-${PythonVer}.tgz
+sudo tar -xzf Python-${PythonVer}.tgz
+cd Python-${PythonVer}/
+sudo ./configure --enable-optimizations
+sudo make altinstall
+python${PythonVerMajor} --version
 
-#Install pip3
+#Step3: Install pip3
 cd ~
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python3.8 get-pip.py --user
-pip3.8 --version
+sudo curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python${PythonVerMajor} get-pip.py --user
+sudo pip${PythonVerMajor} --version
 
-#Install awscli
-pip3.8 install awscli --upgrade --user
+#Step4: Install awscli
+sudo pip${PythonVerMajor} install awscli --upgrade --user
 awscli --version
 
 #Export PATH
-export PATH=~/.local/bin:$PATH
-source ~/.bash_profile
+sudo export PATH=~/.local/bin:$PATH
+sudo source ~/.bash_profile
 
 #Making PATH ready for future session
 echo "PATH=~/.local/bin:$PATH" | tee -a ~/.bash_profile
 echo "export \$PATH" | tee -a ~/.bash_profile
+~
+
